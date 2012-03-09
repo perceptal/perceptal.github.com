ae97ba91cf7357bece86ba3032c44dc1bcb494b4
Johnny Hall <johnny@recipher.co.uk>
2012-03-09 09:17:47 +0000

    Update script/perceptal.js

--- script/perceptal.js
+++ script/perceptal.js
@@ -6,7 +6,7 @@
       target = $('a[name=' + anchor + ']');
       ran = false;
       $('html, body').animate({
-        scrollTop: target.offset().top - 75
+        scrollTop: target.offset().top - 65
       }, 400, function() {
         var offset;
         if (!ran) {
